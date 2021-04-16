fa2f33189f409e962998629d0e26187bcfe3d3e7
Fabian <fabiancpbm@gmail.com>
2021-04-15 22:33:56 -0300

    Refatoração

--- ball.py
+++ ball.py
@@ -3,8 +3,8 @@ from OpenGL.GLU import *
 from OpenGL.GL import *
 import math
 
-n1 = 50
-n2 = 50
+n1 = 150
+n2 = 150
 r = 6
 a = 0
 
@@ -17,16 +17,17 @@ def f(i, j):
     return x, y, z
 
 def draw():
+    global a
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
     glPushMatrix() # Guarda o estado de transformação da matriz atual (matriz M)
     
     # glRotatef(angulo, x, y, z)
-    glRotatef(a,1.0,0.0,0.0) # Fazendo matriz atual N = M * rotação
+    glRotatef(a,0.0,0.0,1.0) # Fazendo matriz atual N = M * rotação
     for i in range(0, n1): #Variação do teta
         #glBegin(GL_POINTS)
         glBegin(GL_QUAD_STRIP)
-        glColor3fv((1,1,0))
+        glColor3fv((i/n1,i/n1,0))
         for j in range(0, n2): # Variação do phi
             x, y, z = f(i, j)
             x1, y1, z1 = f(i + 1, j)
@@ -34,7 +35,12 @@ def draw():
             glVertex3f(x1, y1, z1)
         glEnd()
     glPopMatrix() # Voltando ao estado de transformação antes do glPushMatrix (voltando à matriz M)
+    a+=1
     glutSwapBuffers()
+
+def timer(i):
+    glutPostRedisplay()
+    glutTimerFunc(10,timer,1)
 
 glutInit(sys.argv)
 glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE)
@@ -46,4 +52,5 @@ glEnable(GL_DEPTH_TEST)
 glClearColor(0, 0, 0, 1)
 gluPerspective(45, 800.0 / 600.0, 0.1, 100.0) # Aplica matriz perspectiva ao universa
 glTranslatef(0.0, 0.0, -20) # Empurrando 20 und para dentro da tela do PC
+glutTimerFunc(10, timer, 1)
 glutMainLoop()
